3b7a333bde6cf08225fe09aac987a98b6e47cb85
yahyaanwar <yahya.anw@gmail.com>
2018-06-30 22:20:13 +0700

    Revert "done"
    This reverts commit 6631f0ed87b0cb2eed8981adab3ffe7aae3eaf7b.

--- countdown.js
+++ countdown.js
@@ -2,10 +2,9 @@ const second = 1000,
     minute = second * 60,
     hour = minute * 60,
     day = hour * 24,
-    nextYear = new Date().getFullYear() + 1
+    date = new Date(($_GET.date || '0').replace(/%20/g, ' '))
 
-var date = new Date(($_GET.date || '0').replace(/%20/g, ' '))
-    countDown = date != 'Invalid Date' ? date.getTime() : new Date(0).getTime(),
+let countDown = date != 'Invalid Date' ? date.getTime() : new Date(0).getTime(),
     timer = {},
     x = setInterval(function() {
 
@@ -20,19 +19,14 @@ var date = new Date(($_GET.date || '0').replace(/%20/g, ' '))
         document.getElementById('timer').innerText = timer.days + timer.hours + timer.minutes + timer.seconds;
 
         //do something later when date is reached
-        if (distance <= 0 && distance > -(24*60*60*1000)) {
+        if (distance <= 0) {
             clearInterval(x);
             ondate = true;
             document.getElementById('countdown').innerHTML = '<h1>Selesai</h1>';
         }
 
-        if (distance < -(24*60*60*1000)) {
-            ondate = false;
-            countDown = new Date(($_GET.date || '0').replace(/%20/g, ' ').replace(/\b\d{4}\b/, nextYear)).getTime()
-            if ($_GET.yearly != 'true') {
-                clearInterval(x);
-                document.getElementById('countdown').innerHTML = '<h1>terlambat</h1>';
-            }
-        }
-
     }, second)
+
+if (countDown - new Date().getTime() > 0) {
+    document.getElementById('countdown').style.display = 'block'
+}
